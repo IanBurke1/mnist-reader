f7af3820e8a109f4c4700bbd04f8ad2b3d0f9993
Ian Burke <iano.burke@gmail.com>
2017-09-20 12:54:06 +0100

    bug

---
+++ mnist.py
@@ -0,0 +1,13 @@
+# Author: Ian Burke
+# Module: Emerging Technologies
+# Date: September, 2017
+
+#Problem Sheet: https://emerging-technologies.github.io/problems/digits.html
+
+import gzip
+
+f = gzip.open('t10k-images-idx3-ubyte.gz', 'rb')
+
+#magic = f.read(4)
+
+print(f)

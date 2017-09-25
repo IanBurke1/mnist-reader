da3b105236e0131b16df4cf4cf5df1a0519b3f8d
Ian Burke <iano.burke@gmail.com>
2017-09-25 21:01:53 +0100

    adding magic number, num of images, num of rows and columns in the file

--- mnist.py
+++ mnist.py
@@ -6,8 +6,22 @@
 
 import gzip
 
-with gzip.open('C:/Users/Iano/Downloads/t10k-images-idx3-ubyte.gz', 'rb') as f:
-    f.read()
-    magic = f.read(4)
+#with gzip.open('C:/Users/Iano/Downloads/t10k-images-idx3-ubyte.gz', 'rb') as f:
+f = gzip.open('C:/Users/Iano/Downloads/t10k-labels-idx1-ubyte.gz', 'rb')
+    #f.read()
+magic = f.read(4)
+magic = int.from_bytes(magic, 'big')
 
-    print(magic)
+print("Magic is: ", magic)
+
+noimg = f.read(4) #read how many labels in a file
+noimg = int.from_bytes(noimg, 'big')
+print("images are : ",noimg)
+
+norow = f.read(4) #read how many rows in a file
+norow = int.from_bytes(norow, 'big')
+print("rows are : ",norow)
+
+nocol = f.read(4) #read how many coloumns in a file
+nocol = int.from_bytes(nocol, 'big')
+print("cols are : ",nocol)
